bd619a6d577ef013fb97063bc935d6530dd083e5
Skyneur <skyneuroff@gmail.com>
2024-12-13 17:59:00 +0100

    Renommage de la page Accordion en AboutPage et ajout du composant AboutContent avec un style centré et un padding.

--- src/views/Jokes.vue
+++ src/views/Jokes.vue
@@ -1,16 +1,24 @@
 <template>
-    <div class="accordion">
-      <AccordionComponent />
-    </div>
-  </template>
-  
-  <script>
-  import AccordionComponent from '../components/AccordionComponent.vue'
-  
-  export default {
-    name: 'AccordionPage',
-    components: {
-      AccordionComponent
-    }
+  <div class="jokes">
+    <AboutContent />
+  </div>
+</template>
+
+<script>
+import AboutContent from '../components/AboutContent.vue';
+
+export default {
+  name: 'AboutPage', // Renommez le composant pour qu'il soit multi-mots
+  components: {
+    AboutContent
   }
-  </script>
+}
+</script>
+
+<style scoped>
+.jokes {
+  padding: 20px;
+  text-align: center;
+  margin-top: 60px; /* Ajoutez une marge en haut pour éviter que le contenu soit caché sous la navbar */
+}
+</style>
